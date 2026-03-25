97857a299cb1aafe69ffc8fad8e7ce703ce8e529
Guilherme <onet88905@gmail.com>
2026-03-25 11:18:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,8 @@
 function saudacao(nome) {
-
     if (!nome || nome.trim() === '') {
-
         throw new Error('Nome nao pode estar vazio');
-
     }
-
-    return Ola ${nome}! Bem-vindo ao nosso sistema CI/CD!`;
-
+    return `Ola, ${nome}!`;
 }
 
-function calcular(a, b, operacao) {
-
-    switch(operacao) {
-
-        case 'soma':
-
-            return a + b;
-
-        case 'subtracao':
-
-            return a - b;
-
-        case 'multiplicacao':
-
-            return a * b;
-
-        case 'divisao':
-
-            if (b === 0) throw new Error("Divisao por zero nao sao permitidas");
-
-            return a / b;
-
-        default:
-
-            throw new Error("Operacao nao suportada");
-
-    }
-
-}
-
-// Exportar funcoes para testes (em ambiente Node.js)
-
-if (typeof module !== 'undefined' && module.exports) {
-
-    module.exports = { saudacao, calcular };
-
-}
-
-console.log('Aplicacao carregada com sucesso!');
+console.log("OK");
